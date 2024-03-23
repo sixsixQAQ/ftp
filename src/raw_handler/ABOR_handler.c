1308539946524b68dac6ba59ecf841a0df8b14ee
yangqing <sixqaq@outlook.com>
2024-03-23 21:37:27 +0800

    实现ABOR

--- src/raw_handler/ABOR_handler.c
+++ src/raw_handler/ABOR_handler.c
@@ -1,3 +1,13 @@
+#include "context_data.h"
+#include "ftptool.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 void ABOR_handler(int argc, const char **argv)
 {
+    int fd = get_shared_context()->control_connection_fd;
+    char MODE_CMD[] = "ABOR\r\n";
+    write(fd, MODE_CMD, strlen(MODE_CMD));
+    echo_response(fd);
 }
