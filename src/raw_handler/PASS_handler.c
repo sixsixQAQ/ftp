83db36eb613745787fb555473ad8a54ef5821087
yangqing <sixqaq@outlook.com>
2024-03-23 17:56:32 +0800

    实现USER,PASS,REIN命令

--- src/raw_handler/PASS_handler.c
+++ src/raw_handler/PASS_handler.c
@@ -1,3 +1,18 @@
+#include "context_data.h"
+#include "ftplimits.h"
+#include "ftptool.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 void PASS_handler(int argc, const char **argv)
 {
+    if (argc != 2)
+        return;
+
+    int fd = get_shared_context()->control_connection_fd;
+    char PASS_CMD[CMD_MAX_SIZE];
+    snprintf(PASS_CMD, sizeof(PASS_CMD), "PASS %s\r\n", argv[1]);
+    write(fd, PASS_CMD, strlen(PASS_CMD));
+    echo_response(fd);
 }
