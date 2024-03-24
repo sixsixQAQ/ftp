10671b9f6077532e8bdfd68c0c354a0c1ff99501
yangqing <sixqaq@outlook.com>
2024-03-24 10:45:01 +0800

    实现SITE命令

--- src/raw_handler/SITE_handler.c
+++ src/raw_handler/SITE_handler.c
@@ -1,3 +1,18 @@
+#include "context_data.h"
+#include "ftplimits.h"
+#include "ftptool.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 void SITE_handler(int argc, const char **argv)
 {
+    if (argc != 2)
+        return;
+
+    int fd = get_shared_context()->control_connection_fd;
+    char SITE_CMD[CMD_MAX_SIZE];
+    snprintf(SITE_CMD, sizeof(SITE_CMD), "SITE %s\r\n", argv[1]);
+    write(fd, SITE_CMD, strlen(SITE_CMD));
+    echo_response(fd);
 }
