c53828bbc0c54d99b3ff84fd9995a79d3870c2d0
sixqaq <sixqaq@outlook.com>
2024-04-03 00:06:14 +0800

    update

--- include/FTPResponseUtil.hpp
+++ include/FTPResponseUtil.hpp
@@ -7,17 +7,17 @@
 #include <string>
 #include <vector>
 
-class FTPResponseUtil : public ErrorUtil
-{
+class FTPResponseUtil : public ErrorUtil {
 
-    // "214-The following commands are recognized.\r\n"
-    // " ABOR ACCT ALLO APPE CDUP CWD  DELE EPRT EPSV FEAT HELP LIST MDTM MKD\r\n"
-    // " MODE NLST NOOP OPTS PASS PASV PORT PWD  QUIT REIN REST RETR RMD  RNFR\r\n"
-    // " RNTO SITE SIZE SMNT STAT STOR STOU STRU SYST TYPE USER XCUP XCWD XMKD\r\n"
-    // "214 Help OK.\r\n"
-  public:
-    static std::vector<std::string> getResponse(ControlFd connFd);
-    static std::vector<std::string> echoResponse(ControlFd connFd, std::ostream &out = std::cout);
-    static void PASVResponse(ControlFd connFd, std::string &ip, uint16_t &port);
-    static long sizeResponse(ControlFd connFd);
+	// "214-The following commands are recognized.\r\n"
+	// " ABOR ACCT ALLO APPE CDUP CWD  DELE EPRT EPSV FEAT HELP LIST MDTM MKD\r\n"
+	// " MODE NLST NOOP OPTS PASS PASV PORT PWD  QUIT REIN REST RETR RMD  RNFR\r\n"
+	// " RNTO SITE SIZE SMNT STAT STOR STOU STRU SYST TYPE USER XCUP XCWD XMKD\r\n"
+	// "214 Help OK.\r\n"
+public:
+	// TODO: vector解析多余了一个空字符串。
+	static std::vector<std::string> getResponse (ControlFd connFd);
+	static std::vector<std::string> echoResponse (ControlFd connFd, std::ostream &out = std::cout);
+	static void PASVResponse (ControlFd connFd, std::string &ip, uint16_t &port);
+	static long sizeResponse (ControlFd connFd);
 };
