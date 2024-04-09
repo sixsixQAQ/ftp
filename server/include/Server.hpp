3118fc5bd7f9698299bb1165c95893aeedf62b97
popter <popter@qq.com>
2024-04-09 17:48:33 +0800

    提交

--- server/include/Server.hpp
+++ server/include/Server.hpp
@@ -8,9 +8,55 @@
 class ControlServer : public ErrorUtil {
 public:
 	ControlServer (EasySelect *dispatcher, int ctrlFd, struct sockaddr_in addr);
-	void notifyDataCome ();
 
 private:
 	class Impl;
 	std::shared_ptr<Impl> m_pImpl = nullptr;
+};
+
+class Server : ErrorUtil {
+public:
+	Server()
+	{
+		auto workThread = std::thread (&Server::threadEntry, this);
+		workThread.detach();
+	}
+
+	void addClient (int fd, struct sockaddr_in addr)
+	{
+		dispatcher.stop();
+		dispatcher.addFd (fd, [&] (int fd) { m_dataCome.notify_one(); });
+		dispatcher.start();
+	}
+
+	void removeClient (int fd)
+	{
+		dispatcher.stop();
+		dispatcher.removeFd (fd);
+		dispatcher.start();
+	}
+
+protected:
+	virtual void doWork () = 0;
+private:
+
+	void waitForData ()
+	{
+		std::unique_lock<std::mutex> lock (m_haveDataMutex);
+		m_dataCome.wait (lock, [&] { return m_haveData; });
+		m_haveData = false;
+	}
+
+	void threadEntry ()
+	{
+		while (true) {
+			waitForData();
+			doWork();
+		}
+	}
+
+	std::mutex m_haveDataMutex;
+	EasySelect dispatcher;
+	std::condition_variable m_dataCome;
+	bool m_haveData;
 };
