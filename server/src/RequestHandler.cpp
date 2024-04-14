3d3ac8dd18daab5b009760bb6a7f8f4dc56cfaa7
sixqaq <sixqaq@outlook.com>
2024-04-14 18:44:36 +0800

    update

--- server/src/RequestHandler.cpp
+++ server/src/RequestHandler.cpp
@@ -236,7 +236,7 @@ Handlers::RETR_handler (ClientContext &context, const std::vector<std::string> a
 	FTPUtil::sendCmd (context.ctrlFd, {"150", "File status okay; about to open data connection."});
 	NetUtil::syncLocalToRemote (context.dataFd, realAbsPath);
 	FTPUtil::sendCmd (context.ctrlFd, {"226", "226 Transfer complete."});
-	context.ctrlFd.close();
+	context.dataFd.close();
 }
 
 void
